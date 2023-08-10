a373a6911b0c81bdb2abea54f9df73eb52928388
JD <janardanayachi3@gmail.com>
2023-08-10 12:53:46 +0530

    Fixing stuff

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -5,7 +5,7 @@ const {RegisterUser,LoginUser,logout, updateUser,LoadUser,GetAllUsers,GetUser} =
 
 router.route('/Register').post(RegisterUser);
 router.route('/LogIn').post(LoginUser);
-router.route('/Logout').get(authenticate,logout);
+router.route('/Logout').get(logout);
 router.route('/Me').get(authenticate,LoadUser);
 router.route('/All/:id').get(authenticate,GetAllUsers);
 router.route('/:id').get(authenticate,GetUser);
