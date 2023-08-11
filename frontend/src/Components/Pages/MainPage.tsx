ef7b00ea92de54846e4a3867f2b9cbab2eed8870
JD <janardanayachi3@gmail.com>
2023-08-11 22:25:25 +0530

    Testing Servers

--- frontend/src/Components/Pages/MainPage.tsx
+++ frontend/src/Components/Pages/MainPage.tsx
@@ -14,7 +14,7 @@ import {MessagesData} from '../../Types/UserTypes';
 
 
 
-export const socket = io('8800');
+export const socket = io('http://localhost:8800');
  
 const MainPage = () => {
   const {ShowComponent,setSender} = useContext(MainPageContext);
